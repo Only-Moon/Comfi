57d4ea94a6e854769cfced83ff9263ee4b91fa8d
xxDeveloper <77380166+Murtatrxx@users.noreply.github.com>
2021-09-01 21:32:27 +0300

    Update register.js

--- register.js
+++ register.js
@@ -13,10 +13,10 @@ module.exports = {
      */
     run: async (bot, interaction, args, message) => {
 
-if(interaction.user.id !== interaction.guild.ownerId) return interaction.editReply(`⚔️ | This command can only be used by server owner.`) 
+if(interaction.user.id !== interaction.guild.ownerId) return interaction.editReply(`❌ This command can only be used by server owner.`) 
 await interaction.guild.commands.set([...bot.slashCommands].map(x => x[1])) 
   
-return interaction.editReply("✅ | Slash commands are deployed.")
+return interaction.editReply("✅ Slash commands are registered successfully.")
 
 
     }}
