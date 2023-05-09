0632523ed8e36566a6fa360a5969f46ae998a701
Xx-Mohit-xX <75924613+Xx-Mohit-xX@users.noreply.github.com>
2023-05-09 21:41:46 +0530

    Update Comfi.js

--- utils/Comfi.js
+++ utils/Comfi.js
@@ -25,7 +25,7 @@ class Comfi extends Discord.Client {
 					},
 					users: {
 						interval: 3600, // Every hour...
-						filter: user => !!user?.bot && user?.id !== this?.user?.id, // Remove all bots.
+						filter: () => user => user?.bot && user.id !== user?.client.user?.id, // Remove all bots.
 					},
 				},
 			failIfNotExists: true,
